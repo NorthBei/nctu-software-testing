0664a5ab5f3a0fa1dab398fb66c8779b92f26471
NorthBei <seekingfor520@gmail.com>
2018-04-11 23:06:20 +0800

    hw1-triangle , ECT - v1 : code , test - all passed
    第1次撰寫好ECT的程式碼，並進行測試，BVT+ECT2的test case全數通過

--- hw1/Triangle/triangle.cc
+++ hw1/Triangle/triangle.cc
@@ -1,9 +1,11 @@
 #include "triangle.h"
 #include <string>
+#include<vector>
 
 using namespace std; 
 
 bool isRangeValid(int slide){
+	//輸入的數值要在規範(1~200)內
 	return (slide >= 1) && (slide <= 200) ? true : false;
 }
 
@@ -11,10 +13,28 @@ bool isSlideValid(int a,int b,int c){
 	return a<(b+c);
 }
 
-string triangleType(int a,int b,int c){
+vector<string> isInputValid(int a,int b,int c){
+	vector<string> array;
 
-	if(!isRangeValid(a) || !isRangeValid(b) || !isRangeValid(c) || !isSlideValid(a,b,c) || !isSlideValid(b,a,c) || !isSlideValid(c,b,a)){
-		//輸入的數值要在規範(1~200)內
+	if(!isRangeValid(a)){array.push_back("a");}
+	if(!isRangeValid(b)){array.push_back("b");}
+	if(!isRangeValid(c)){array.push_back("c");}
+
+	return array;
+}
+
+string triangleType(int a,int b,int c){
+	vector<string> list = isInputValid(a,b,c);
+	if(!list.empty()){
+		string slides;
+		int size = list.size();
+		for(int i = 0; i< size;i++){
+			slides += (i == size-1 ? list[i] : list[i]+",");
+		}
+		return "Value of "+slides+" is not in he range of permitted values";
+	}
+
+	if(!isSlideValid(a,b,c) || !isSlideValid(b,a,c) || !isSlideValid(c,b,a)){
 		return "Not a triangle";
 	}
 
--- hw1/Triangle/triangle.h
+++ hw1/Triangle/triangle.h
@@ -1,4 +1,5 @@
 #include <string>
+#include<vector>
 using namespace std; 
 
 #ifndef GTEST_TRIANGLE_H_
