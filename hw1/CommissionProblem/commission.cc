292192942e7500c8f1bc13ceba2804ad780e7509
NorthBei <seekingfor520@gmail.com>
2018-04-13 01:19:12 +0800

    hw1-commission,- v1 : code , test - all passed
    程式碼撰寫完成，並進行BVT+ECT+DTT測試，test case全數通過

--- hw1/CommissionProblem/commission.cc
+++ hw1/CommissionProblem/commission.cc
@@ -5,5 +5,33 @@
 using namespace std; 
 
 string calcCommission(int locks, int stocks, int barrels){
-	return "";
+	const float LOCK_PRICE = 45.0f;
+	const float STOCK_PRICE = 30.0f;
+	const float BARREL_PRICE = 25.0f;
+
+    float result;
+    
+    if(locks == -1)
+        return "Program terminates";
+    if( (locks < 1 || locks > 70) || (stocks < 1 || stocks > 80) || (barrels < 1 || barrels > 90))
+        return "Invalid items' amount";
+
+    float lockSales = locks*LOCK_PRICE;
+    float stockSales = STOCK_PRICE * stocks;
+    float barrelSales = BARREL_PRICE * barrels;
+    float sales = lockSales + stockSales + barrelSales;
+
+    if(sales > 1800.0){
+        result = 0.10*1000.0 + 0.15*800.0 + 0.2*(sales - 1800.0);
+    }
+    else if(sales > 1000.0){
+        result = 0.10*1000.0 + 0.15*(sales - 1000.0);
+    }
+    else{
+        result = 0.10*sales;
+    }
+    
+    stringstream ss;
+	ss << result;
+    return ss.str();
 }
