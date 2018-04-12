593ec89cd130e432c30c08c6fbfb7383e2c1af71
NorthBei <seekingfor520@gmail.com>
2018-04-13 00:57:58 +0800

    hw1-commission modify define
    更改function parameter & include more std class

--- hw1/CommissionProblem/commission.cc
+++ hw1/CommissionProblem/commission.cc
@@ -1,4 +1,9 @@
 #include "commission.h"
+#include <sstream>
 #include <string>
 
 using namespace std; 
+
+string calcCommission(int locks, int stocks, int barrels){
+	return "";
+}
--- hw1/CommissionProblem/commission.h
+++ hw1/CommissionProblem/commission.h
@@ -1,10 +1,11 @@
 #include <string>
+#include <sstream>
 using namespace std; 
 
 #ifndef GTEST_COMMISSION_H_
 #define GTEST_COMMISSION_H_
 
 // Returns the money earned by seller
-int calcCommission(int numA,int numB ,int numC);
+string calcCommission(int locks, int stocks, int barrels);
 
 #endif  // GTEST_COMMISSION_H_
